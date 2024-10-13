8c2159b8da3af2d1d0d2ca7a93970ed9c76656be
kirya vyalov <scriptythememer@gmail.com>
2024-10-13 14:25:40 +0700

    pretty cool

---
+++ exampletext.lua
@@ -0,0 +1,19 @@
+local var = true
+local num = 1
+local string = "string"
+
+function example(lol, lol)
+    local operator = num + 1
+    operator = -operator
+
+    if not operator or var then return end
+
+    return {"", ""}
+end
+
+--[[
+    sadaadssa
+]]
+
+local metatable = {}
+metatable.Sup = print("hello world")
